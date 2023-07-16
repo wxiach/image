9f7b77e4a1582ee38c58ac0b5df2961596e29e4e
0914xc <weixiaochen0914@gmail.com>
2023-07-16 21:17:10 +0800

    add images

---
+++ source/image.sh
@@ -0,0 +1,24 @@
+!/bin/bash
+
+for name in ./*; do
+    ext="${name##*.}"
+    lowercase_ext=$(echo "$ext" | tr '[:upper:]' '[:lower:]')
+    if [[ "$lowercase_ext" == "jpg" || 
+          "$lowercase_ext" == "jpeg" || 
+          "$lowercase_ext" == "png" || 
+          "$lowercase_ext" == "gif" || 
+          "$lowercase_ext" == "bmp" || 
+          "$lowercase_ext" == "tiff" || 
+          "$lowercase_ext" == "svg" ]]; then
+        rename=`md5sum "$name" | awk '{print $1}'`
+        mv "$name" ../${rename}.$lowercase_ext
+    fi
+done
+
+git pull
+
+git add .
+
+git commit -m 'add images'
+
+git push
